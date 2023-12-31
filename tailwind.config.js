2c001a333e9f559269123864d7dc5b446a1aeb2b
jakiamunni <jakiasmunni205@gmail.com>
2023-12-31 18:19:04 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        primary: "#B4975A",
+        secondary: "#8B0000",
+        lightDark: "#24252e",
+        dark: "#333333"
+      }
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+      },
+    },
+  },
+  plugins: [require("daisyui")],
+}
